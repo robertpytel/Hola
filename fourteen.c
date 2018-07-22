e602587189446146dcb171bbb2c0e118c23705c9
robertpytel <Robert.Pytel@kpit.com>
2018-07-22 14:15:03 -0400

    Adds 5 to 14
    This is an example program that serves as the second file to be uploaded to the Hola repo. It uses the function "add5(int)" to add 5 to the number 14.

---
+++ fourteen.c
@@ -0,0 +1,18 @@
+﻿#include <iostream.h>
+
+//This is the second file being uploaded to github
+//I still need to look up a case study for .c files
+
+int main()
+{
+  int fourteen = 14;
+  fourteen = add5(fourteen);
+  cout << "fourteen is: " << fourteen << endl;
+  return 0;
+}
+
+int add5(int &addTo)
+{
+  addTo+=5;
+  return addTo;
+}
