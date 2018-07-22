bbd867dd23cebb66ccd21962996102f284b9b8e4
robertpytel <Robert.Pytel@kpit.com>
2018-07-22 14:04:20 -0400

    This is a first file to commit on github
    Instead of Hello World, it is Hello Amigo written as Hola Amigo

---
+++ hola.c
@@ -0,0 +1,11 @@
+﻿#include <iostream.h>
+
+// This is a test from Robert Pytel
+// 7/22/2018
+
+int main()
+{
+  cout << "Hola World!" << endl;
+  cout << "This is a test for github" << endl;
+  return 0;
+}
