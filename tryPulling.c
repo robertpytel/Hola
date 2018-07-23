926dba5458c7bf16f6f3f753c4e6fa1b0e12aee7
robertpytel <Robert.Pytel@kpit.com>
2018-07-22 20:29:49 -0400

    This will be pulled back into the master branch from masterCopy
    This file shoul undergo a pull request from masterCopy after committing to masterCopy.

---
+++ tryPulling.c
@@ -0,0 +1,10 @@
+﻿#include <iostream.h>
+
+//This is to test branching
+//Robert Pytel 7/22/2018
+
+int main()
+{
+  cout << "This is a branch that will undergo a pull request." << endl;
+  return 0;
+}
